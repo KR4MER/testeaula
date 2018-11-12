997dcfae984bea57da24982aec79ef4bcb40a1e6
5220113 <5220113@D98935.farroupilha.ifrs.edu.br>
2018-11-12 19:43:36 -0300

    ertert

---
+++ src/message/Message.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package message;
+
+/**
+ *
+ * @author 5220113
+ */
+public class Message {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Message1");
+        System.out.println("Message2");
+    }
+    
+}
